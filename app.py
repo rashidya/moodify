c8e5c70247239b482339f12183be1765e8236efe
rashidya <rashmiliyanage2000@gmail.com>
2023-05-18 22:02:56 +0530

    Emotion input added

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.title("Moodify")
+# Display the app name as a title
+title_text = "<h1 style='color: #1B9C85;'>Moodify🎧</h1>"
+st.markdown(title_text, unsafe_allow_html=True)
+
+# Introduction or tagline
+description_text = "<span style='color: #4C4C6D;'>Unleash the Power of Music to Match Your Mood</span>"
+st.markdown(f"<h6>{description_text}</h6>", unsafe_allow_html=True)
+
+# Emotion Input
+emotion = st.selectbox("Select your current emotion", ["Happy😍", "Sad🥺", "Angry😤", "Relaxed😊"])
+
