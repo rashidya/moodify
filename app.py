5816b24073f1827d266c0498851097f94176d8ba
rashidya <rashmiliyanage2000@gmail.com>
2023-06-24 00:29:01 +0530

    filter added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit.components.v1 as components
 from flask import Flask
 import mysql.connector
+import random
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'moodify-database.c3kt6gsj5iwk.ap-southeast-2.rds.amazonaws.com'
@@ -20,9 +21,9 @@ conn = mysql.connector.connect(
 
 
 # Create the login form 
-def home(name): 
+def home(): 
 
-    st.markdown(f"<h4>Hi {name}</h4>", unsafe_allow_html=True)
+   # st.markdown(f"<h4>Hi {name}</h4>", unsafe_allow_html=True)
     # Display the app name as a title
     title_text = "<h1 style='color: #1B9C85;'>Moodify🎧</h1>"
     st.markdown(title_text, unsafe_allow_html=True)
@@ -50,17 +51,33 @@ def home(name):
     # Load the dataset into a Pandas DataFrame
     df = pd.read_csv("data/dataset.csv")
 
-    # Retrieve songs based on genre
-    desired_genre = "Pop"  # Replace with the desired genre
-    songs_by_genre = df[df["genre"] == desired_genre]
 
+
+    #Happy Genre
+    happy_mood =['dnb','hardstyle','RnB']
+    sad_mood =['Emo','RnB','trance','psytrance']
+    angry_mood = ['Hiphop','Dark Trap','Underground Rap','Trap Metal','Rap','trap']
+    relaxed_mood = ['Rap','RnB','techhouse','techno','trance','psytrance','hardstyle']
+
+    desired_genre = happy_mood
+    # Retrieve songs based on genre
+    if emotion == "Happy😍":
+        desired_genre=happy_mood
+    if emotion ==  "Sad🥺":
+        desired_genre=sad_mood
+    if emotion ==  "Angry😤":
+        desired_genre=angry_mood
+    if emotion == "Relaxed😊":
+        desired_genre=relaxed_mood
+    
+    # Replace with the desired genre
+    songs_by_genre = df[df["genre"].isin(desired_genre)]
 
     # Create an empty list to store the song URLs
     song_urls = []
 
     # Iterate over the songs in the genre
     for index, song in songs_by_genre.iterrows():
-        if index<18060:
             # Define the Spotify track URI
             track_uri = song["uri"]
 
@@ -73,12 +90,12 @@ def home(name):
             # Add the Spotify player code to the list
             song_urls.append(spotify_player_code)
             
-
+    random.shuffle(song_urls)
     num_cols = 2
-    num_rows = len(songs_by_genre) // num_cols + 1
-
+    # num_rows = len(songs_by_genre) // num_cols + 1
+    num_rows = 10
     # Split the song_urls list into chunks based on the number of columns
-    chunks = [song_urls[i:i + num_cols] for i in range(0, len(song_urls), num_cols)]
+    chunks = [song_urls[i:i + num_cols] for i in range(0, 20, num_cols)]
 
     # Display the song previews in a grid
     for chunk in chunks:
@@ -89,62 +106,65 @@ def home(name):
 
 
 # Create the login form 
-def login_form(): 
+# def login_form(): 
     
     
-    # Initialize session state
-    if "button_state" not in st.session_state:
-        st.session_state.button_state = {"Sign Up": False}
+#     # Initialize session state
+#     if "button_state" not in st.session_state:
+#         st.session_state.button_state = {"Sign Up": False}
 
-    # Change the label of Button A based on Button B state
-    if st.session_state.button_state["Sign Up"]:
-        button_a_label = "Register"
-        button_b_label = "Log In"
-        msg="Already have an account ?"
-    else:
-        button_a_label = "Log In"
-        button_b_label = "Register"
-        msg="Don\'t have an account?"
+#     # Change the label of Button A based on Button B state
+#     if st.session_state.button_state["Sign Up"]:
+#         button_a_label = "Register"
+#         button_b_label = "Log In"
+#         msg="Already have an account ?"
+#     else:
+#         button_a_label = "Log In"
+#         button_b_label = "Register"
+#         msg="Don\'t have an account?"
 
-    # Display the app name as a title
-    title_text = "<h1 style='color: #1B9C85; text-align:center;'>Moodify🎧</h1>"
-    st.markdown(title_text, unsafe_allow_html=True)
+#     # Display the app name as a title
+#     title_text = "<h1 style='color: #1B9C85; text-align:center;'>Moodify🎧</h1>"
+#     st.markdown(title_text, unsafe_allow_html=True)
 
-    # Get the username and password from the user 
-    username = st.text_input("Username",key="username") 
-    password = st.text_input("Password", type='password',key="password") 
+#     # Get the username and password from the user 
+#     username = st.text_input("Username",key="username") 
+#     password = st.text_input("Password", type='password',key="password") 
  
  
-    submit = st.button(button_a_label) 
+#     submit = st.button(button_a_label) 
        
 
-    st.write(msg) 
-    if st.button(button_b_label):
-         if st.session_state.button_state["Sign Up"] :
-            st.session_state.button_state["Sign Up"] = False
-         else :
-            st.session_state.button_state["Sign Up"] = True
-    cursor = conn.cursor() 
+#     st.write(msg) 
+#     if st.button(button_b_label):
+#          if st.session_state.button_state["Sign Up"] :
+#             st.session_state.button_state["Sign Up"] = False
+#          else :
+#             st.session_state.button_state["Sign Up"] = True
+#     cursor = conn.cursor() 
 
-    # Check if the login button is clicked 
-    if submit: 
-        if button_a_label == "Log In":
-            query = 'SELECT * FROM user WHERE username=%s AND password=%s' 
-            values = (username,password) 
-            cursor.execute(query, values) 
-            response = cursor.fetchone() 
-                # Perform login authentication and user validation here 
-                # Replace the condition with your actual login logic 
-            if response : 
-                    st.success('Logged in successfully!') 
-                    st.session_state.page = "home"
-                    home(response[1])
-            else: 
-                    st.error('Invalid credentials') 
+#     # Check if the login button is clicked 
+#     if submit and username!="" and password !="": 
+#         if button_a_label == "Log In" :
+#             query = 'SELECT * FROM user WHERE username=%s AND password=%s' 
+#             values = (username,password) 
+#             cursor.execute(query, values) 
+#             response = cursor.fetchone() 
+#                 # Perform login authentication and user validation here 
+#                 # Replace the condition with your actual login logic 
+#             if response : 
+#                     st.success('Logged in successfully!') 
+#                     st.session_state.page = "home"
+#                     home(response[1])
+#             else: 
+#                     st.error('Invalid credentials') 
 
-        else :
-            cursor.execute('INSERT INTO user (username , password ) VALUES (%s, %s)', (username,password))
-            conn.commit()
-            st.success('Registered successfully!') 
-
-login_form()
+#         else :
+#             cursor.execute('INSERT INTO user (username , password ) VALUES (%s, %s)', (username,password))
+#             conn.commit()
+#             username=""
+#             password=""
+#             st.success('Registered successfully!') 
+        
+# login_form()
+home()
